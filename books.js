02eceb090aa280e9c64e8cc6d8afd408faeda2cd
KoleKnapp <koleknapp@yahoo.com>
2024-07-11 17:54:39 -0600

    Create books.js

---
+++ books.js
@@ -0,0 +1,48 @@
+// Function to find an author by their ID
+function findAuthorById(authors, id) {
+  return authors.find(author => author.id === id);
+}
+
+// Function to find a book by its ID
+function findBookById(books, id) {
+  return books.find(book => book.id === id);
+}
+
+// NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
+function partitionBooksByBorrowedStatus(books) {
+  return books.reduce(
+    (acc, book) => {
+      const [borrowed, returned] = acc;
+      const recent = book.borrows[0];
+      if (recent.returned) {
+        returned.push(book);
+      } else {
+        borrowed.push(book);
+      }
+
+      return acc;
+    },
+    [[], []]
+  );
+}
+
+function getBorrowersForBook(book, accounts) {
+  const accountsById = accounts.reduce((acc, account) => {
+    acc[account.id] = account;
+    return acc;
+  }, {});
+
+  return book.borrows
+    .map(({ id, returned }) => ({
+      ...accountsById[id],
+      returned,
+    }))
+    .slice(0, 10);
+}
+
+module.exports = {
+  findAuthorById,
+  findBookById,
+  partitionBooksByBorrowedStatus,
+  getBorrowersForBook,
+};
